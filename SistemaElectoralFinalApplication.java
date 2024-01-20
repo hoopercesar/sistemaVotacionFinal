a9a8cd1b20b6291d69e790b3cf755ee3cdd77c79
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-01-20 13:05:23 -0300

    Add files via upload

---
+++ SistemaElectoralFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaElectoralFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaElectoralFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaElectoralFinalApplication.class, args);
+	}
+
+}
